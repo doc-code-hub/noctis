c52fd9a8cb84b507a68728340d7a2c5e16491098
Doc-code <bellini.francescob@gmail.com>
2021-06-24 01:16:25 +0200

    Version 1.0.7

--- src/colors.mjs
+++ src/colors.mjs
@@ -174,5 +174,21 @@ export default {
       support: "#49d6e9",
       misc: "#49ace9",
       invalid: "#e3541c"
+   },
+   redflow: {
+      comment: "#8b747c",
+      text: "#cbbec2",
+      keyword: "#df769b",
+      variable: "#e4b781",
+      annotation: "#d67e5c",
+      constant: "#d5971a",
+      tag: "#e66533",
+      string: "#49e9a6",
+      stringInterpolated: "#16b673",
+      number: "#7060eb",
+      function: "#16a3b6",
+      support: "#49d6e9",
+      misc: "#49ace9",
+      invalid: "#e3541c"
    }
 };
